45476e6788e4393ccb0ce313e3f8f78c1594cad8
kzares-dev <138066850+kzares-dev@users.noreply.github.com>
2024-04-16 11:29:29 -0400

    implemented drag & drop functionality

--- components/main/TerminalBox.tsx
+++ components/main/TerminalBox.tsx
@@ -7,9 +7,11 @@ import Help from "./commands/Help";
 import EnteredCmd from "./EnteredCmd";
 import CmdUserInput from "./CmdUserInput";
 import renderCmd from "@/lib/renderCmd"
+import { Rnd } from "react-rnd"
 
 export default function TerminalBox() {
-    const [enteredCmd, setEnteredCmd] = useState([
+	// This state is the container of all commands
+	const [enteredCmd, setEnteredCmd] = useState([
 		{
 			cmd: "",
 			Component: Welcome,
@@ -21,13 +23,13 @@ export default function TerminalBox() {
 			time: new Date().toLocaleTimeString(),
 		},
 	]);
+
+	// scroll to bottom
 	const dummyRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-    useEffect(() => {
+	useEffect(() => {
 		dummyRef.current.scrollIntoView({ behavior: "auto" });
 	}, [enteredCmd]);
-	useEffect(() => {
-		document.body.addEventListener("keydown", handleKeyEvent);
-	}, []);
+	// simple handle submit 
 	const handleSubmit = (cmd: string) => {
 		setEnteredCmd((currentCmd) => [
 			...currentCmd,
@@ -35,22 +37,62 @@ export default function TerminalBox() {
 		]);
 	};
 
+	// watch the combination of crl + l to clear the terminal
+	useEffect(() => {
+		document.body.addEventListener("keydown", handleKeyEvent);
+	}, []);
 	const handleKeyEvent = (e: KeyboardEvent) => {
 		if (e.ctrlKey && e.key.toLocaleLowerCase() === "l") {
 			setEnteredCmd([]);
 		}
 	};
-    return (
-        <div>
-            <Navbar />
-            <div className="max-w-4xl border-x-2 border-b-2 border-slate-800 rounded-b-md mx-auto text-gray-300 text-xl p-2 overflow-y-auto h-55vh bg-black bg-opacity-75 box">
-                <TodayDate />
-                <EnteredCmd enteredCmd={enteredCmd} />
-                <CmdUserInput onSubmit={handleSubmit} />
+	// Track the screen size to put the terminal in center
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight
+	});
+
+	const elementWidth = 896; // Ancho del elemento que deseas centrar
+	const elementHeight = windowSize.height/2; // Alto del elemento que deseas centrar
+
+	const centerX = (windowSize.width - elementWidth) / 2;
+	const centerY = -windowSize.height/4;
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight
+			});
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	return (
+		<Rnd
+			default={{
+				x: centerX,
+				y: centerY,
+				width: "896",
+				height: "0",
+			}}
+		>
+			<Navbar />
+			<div className="max-w-4xl border-x-2 border-b-2 border-slate-800 rounded-b-md mx-auto text-gray-300 text-xl p-2 overflow-y-auto h-50vh bg-black bg-opacity-75 box">
+				<TodayDate />
+				<EnteredCmd enteredCmd={enteredCmd} />
+				<CmdUserInput onSubmit={handleSubmit} />
 				<div ref={dummyRef}></div>
 
 
-            </div>
-        </div>
-    );
+			</div>
+		</Rnd>
+
+
+	);
 }
